5fabb1d2149f6f29d2e3f6f59a03ee5f59d711ed
wjwj910 <wjwj910@gmail.com>
2025-01-09 14:23:11 +0900

    author info on write 2

--- src/main/java/com/ll/auth/domain/member/member/service/MemberService.java
+++ src/main/java/com/ll/auth/domain/member/member/service/MemberService.java
@@ -37,4 +37,8 @@ public class MemberService {
     public Optional<Member> findByUsername(String username) {
         return memberRepository.findByUsername(username);
     }
+
+    public Optional<Member> findById(long authorId) {
+        return memberRepository.findById(authorId);
+    }
 }
--- src/main/java/com/ll/auth/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/ll/auth/domain/post/post/controller/ApiV1PostController.java
@@ -91,7 +91,7 @@ public class ApiV1PostController {
     public RsData<PostWriteResBody> writeItem(
             @RequestBody @Valid PostWriteReqBody reqBody
     ) {
-        Member actor = memberService.findByUsername("user3").get();
+        Member actor = memberService.findById(reqBody.authorId).get();
 
         Post post = postService.write(actor, reqBody.title, reqBody.content);
 
