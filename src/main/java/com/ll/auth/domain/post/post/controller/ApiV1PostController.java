4ca5bfbfaed69cbe0d8e9685ab60752986f1b30d
wjwj910 <wjwj910@gmail.com>
2025-01-09 16:51:06 +0900

    request scope 2

--- src/main/java/com/ll/auth/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/ll/auth/domain/post/post/controller/ApiV1PostController.java
@@ -23,8 +23,10 @@ import java.util.List;
 public class ApiV1PostController {
     private final PostService postService;
     private final MemberService memberService;
+    private final HttpServletRequest request;
 
-    private Member checkAuthentication(String credentials) {
+    private Member checkAuthentication() {
+        String credentials = request.getHeader("Authorization");
         credentials = credentials.substring("Bearer ".length());
         String[] credentialsBits = credentials.split("/", 2);
         long actorId = Long.parseLong(credentialsBits[0]);
@@ -55,10 +57,8 @@ public class ApiV1PostController {
     }
 
     @DeleteMapping("/{id}")
-    public RsData<Void> deleteItem(@PathVariable long id,
-                                   HttpServletRequest request) {
-        String credentials = request.getHeader("Authorization");
-        Member actor = checkAuthentication(credentials);
+    public RsData<Void> deleteItem(@PathVariable long id) {
+        Member actor = checkAuthentication();
 
         Post post = postService.findById(id).get();
 
@@ -87,9 +87,8 @@ public class ApiV1PostController {
     @PutMapping("/{id}")
     @Transactional
     public RsData<PostDto> modifyItem(@PathVariable long id,
-                                      @RequestBody @Valid PostModifyReqBody reqBody,
-                                      @RequestHeader("Authorization") String credentials) {
-        Member actor = checkAuthentication(credentials);
+                                      @RequestBody @Valid PostModifyReqBody reqBody) {
+        Member actor = checkAuthentication();
 
         Post post = postService.findById(id).get();
 
@@ -116,10 +115,9 @@ public class ApiV1PostController {
 
     @PostMapping
     public RsData<PostDto> writeItem(
-            @RequestBody @Valid PostWriteReqBody reqBody,
-            @RequestHeader("Authorization") String credentials
+            @RequestBody @Valid PostWriteReqBody reqBody
     ) {
-        Member actor = checkAuthentication(credentials);
+        Member actor = checkAuthentication();
 
         Post post = postService.write(actor, reqBody.title, reqBody.content);
 
